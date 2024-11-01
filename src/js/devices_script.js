f210b1988e376571a8f644fa67e45ef39c53ae7e
Danila Guryanov <forgustust@gmail.com>
2024-11-02 01:42:47 +0300

    swiper prices devices comeccian offer

---
+++ src/js/devices_script.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+new Swiper('.devices__swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickTable: true ,
+    },
+    spaceBetween: 16,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.3,
+      },
+      375: {
+        slidesPerView: 1.5,
+      },
+  
+      425: {
+        slidesPerView: 1.7,
+      },
+  
+      600: {
+        slidesPerView: 2.4,
+      }
+    },
+  });
+  
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
 import '../scss/main.scss';
 import '../html/index.html';
 import './brands_script';
+import './devices_script';
+import './prices_script';
