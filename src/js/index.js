749501c1197af47d1c864a960180ff696642f5f0
Danila Guryanov <forgustust@gmail.com>
2024-11-03 00:58:40 +0300

    compited 320px

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,5 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
 import '../scss/main.scss';
 import '../html/index.html';
-import './brands_script';
-import './devices_script';
-import './prices_script';
+import './brands-script';
+import './devices-script';
+import './prices-script';
