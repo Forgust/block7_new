a76786539f573be796bd10b21f6d47dc66c0f78b
Danila Guryanov <forgustust@gmail.com>
2024-11-08 01:05:11 +0300

    added right side menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import '../html/side_menu.html';
 import '../html/index.html';
 import './swipers';
 
@@ -7,7 +8,6 @@ let devicesMoreButton = document.querySelector('.devices__button');
 
 let brandsList = document.querySelectorAll('.brands-slide');
 let devicesList = document.querySelectorAll('.devices-slide');
-
 
 
 function showMore (slidsList, button) {
@@ -33,20 +33,26 @@ devicesMoreButton.onclick = function () {
   showMore(devicesList, devicesMoreButton);
 }
 
-function desctopHiden () {
+function desctopHiden (slidsList) {
     for (let i = 0; i < slidsList.length; i++) {
-        if (window.innerWidth >= 1440 && slidsList[i].classList.contains('desctop')) {
+        if (window.innerWidth >= 1120 && slidsList[i].classList.contains('desctop') && slidsList[i].classList.contains('hiden-slide')) {
             slidsList[i].classList.remove('hiden-slide');
             slidsList[i].classList.add('not-hiden-desctop');
-        } else if (window.innerWidth < 1440 && slidsList[i].classList.contains('not-hiden-desctop')) {
+        } else if (window.innerWidth < 1120 && slidsList[i].classList.contains('not-hiden-desctop')) {
             slidsList[i].classList.remove('not-hiden-desctop');
             slidsList[i].classList.add('hiden-slide');
       };
     };
 };
 
+desctopHiden(brandsList);
+desctopHiden(devicesList);
 
-
+window.addEventListener('resize', function () {
+  console.log(window.innerWidth);
+  desctopHiden(brandsList);
+  desctopHiden(devicesList);
+});
 
 
 
